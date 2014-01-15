aa7a19b6981492cae2f45d7c221e59b0e9d7e691
chunqishi <shicq@cs.brandeis.edu>
2014-01-15 07:30:55 -0500

    Third party by source

---
+++ src/python/thirdparty/pyth/__init__.py
@@ -0,0 +1,10 @@
+################################################################################
+# Name     : __init__.py                                                       #
+# Brief    : import pyth from 2014-01-15.                                      #
+#            for compiling, git clone https://github.com/brendonh/pyth.git     #
+#            into thirdparty module directory                                  #
+# Author   : Chunqi SHI <diligence.cs@gmail.com>                               #
+# Copyright: &copy 2013 ~ present Chunqi SHI   <diligence.cs@gmail.com>        #
+################################################################################
+
+__all__ = ['pyth']
