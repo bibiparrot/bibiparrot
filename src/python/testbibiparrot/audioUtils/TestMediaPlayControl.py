668f17173f1cda003904527e427d8ffd538d15a9
chunqishi <shicq@cs.brandeis.edu>
2014-01-22 13:55:25 -0500

    Improve test mediaplayer

--- src/python/testbibiparrot/audioUtils/TestMediaPlayControl.py
+++ src/python/testbibiparrot/audioUtils/TestMediaPlayControl.py
@@ -28,30 +28,47 @@ class TestMediaPlayControl(unittest.TestCase):
         pass
 
     def testCtrl(self):
-        self.ctrl = PygameMediaPlayCtrl()
-        for i in range(1, 10):
-            print '--------------------->', i
-            self.ctrl.load()
-            self.ctrl.open(self.mp3Path)
-            self.ctrl.start(50)
-            # self.ctrl = PygameMediaPlayCtrl()
-            # while self.ctrl.isPlayed():
-            #     time.sleep(0.05)
-            # self.ctrl.start(60)
-            time.sleep(4)
-            print 'get_pos', self.ctrl.player.get_pos()
-            # self.ctrl.pause()
-            # time.sleep(3)
-            # self.ctrl.resume()
-            # time.sleep(3)
-            # self.ctrl.stop()
-            # time.sleep(2)
-            # while self.ctrl.loaded:
-            #     print '.'
-            #     time.sleep(0.1)
-            # time.sleep(2)
-            self.ctrl.stop()
-            self.ctrl.quit()
+        # self.ctrl = PygameMediaPlayCtrl()
+        # for i in range(1, 10):
+        #     print '--------------------->', i
+        #     self.ctrl.load()
+        #     self.ctrl.open(self.mp3Path)
+        #     self.ctrl.start(50)
+        #     # self.ctrl = PygameMediaPlayCtrl()
+        #     # while self.ctrl.isPlayed():
+        #     #     time.sleep(0.05)
+        #     # self.ctrl.start(60)
+        #     time.sleep(4)
+        #     print 'get_pos', self.ctrl.player.get_pos()
+        #     # self.ctrl.pause()
+        #     # time.sleep(3)
+        #     # self.ctrl.resume()
+        #     # time.sleep(3)
+        #     # self.ctrl.stop()
+        #     # time.sleep(2)
+        #     # while self.ctrl.loaded:
+        #     #     print '.'
+        #     #     time.sleep(0.1)
+        #     # time.sleep(2)
+        #     self.ctrl.stop()
+        #     self.ctrl.quit()
+
+        import pygame, inspect
+        ### http://www.pygame.org/docs/ref/display.html ###
+        pygame.display.init()
+        screen = pygame.display.set_mode((1024, 768))
+        background = pygame.Surface((1024, 768))
+        screen.blit(background, (0, 0))
+        pygame.display.update()
+        movpath = os.path.join(os.path.dirname(inspect.getfile(inspect.currentframe())), 'xiaoaojianghu.flv')
+        mov = pygame.movie.Movie(movpath)
+        mrect = pygame.Rect(0,0,140,113)
+        mov.set_display(screen, mrect.move(65, 150))
+        mov.set_volume(0)
+        mov.play()
+        time.sleep(6)
+        mov.stop()
+        pygame.display.quit()
         pass
 
 
